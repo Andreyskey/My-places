1bbe4be8447e8e31f77cc9c372020999f518e2f5
Andrei Volkov <be0rn99@icloud.com>
2025-03-06 18:21:05 +0100

    Initial Commit

---
+++ My places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  My places
+//
+//  Created by Andrej Vuković on 6.3.25..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
